d0d1ef8ef7e9e0df1c404ce8b150fb66613bface
QwErTy-yTrEwQ03 <maks-veselov-2003@mail.ru>
2023-05-06 18:56:16 +0300

    Добавил slq с тестовыми данными

---
+++ data/example/pizzas.sql
@@ -0,0 +1,10 @@
+INSERT INTO pizza (name, description, category, price, img_path)
+VALUES ('Pizza 1', 'Best pizza at the world', 'hit', 150.2, 'pizza.png'),
+      ('Pizza 2', 'Best pizza at the world', 'hit', 100, 'pizza.png'),
+      ('Pizza 3', 'Best pizza at the world', 'hit', 200.5, 'pizza.png'),
+      ('Pizza 4', 'Best pizza at the world', 'season', 90.9, 'pizza.png'),
+      ('Pizza 5', 'Best pizza at the world', 'season', 150, 'pizza.png'),
+      ('Pizza 6', 'Best pizza at the world', 'season', 320, 'pizza.png'),
+      ('Pizza 7', 'Best pizza at the world', 'season', 190, 'pizza.png'),
+      ('Pizza 8', 'Best pizza at the world', 'season', 190, 'pizza.png'),
+      ('Pizza 9', 'Best pizza at the world', 'season', 40, 'pizza.png');
---
+++ data/example/users.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (email, name)
+VALUES ('petro-petr-2000@yandex.ru', 'Petr')
