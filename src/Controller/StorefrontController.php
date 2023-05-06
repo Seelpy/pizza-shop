9cdfb10257bd10a0ccc07cc8569a5e531898493f
QwErTy-yTrEwQ03 <maks-veselov-2003@mail.ru>
2023-05-06 20:47:55 +0300

    Добавил сессии

--- src/Controller/StorefrontController.php
+++ src/Controller/StorefrontController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
@@ -6,6 +7,7 @@ namespace App\Controller;
 
 use App\Database\ConnectionProvider;
 use App\Database\PizzaTable;
+use App\Database\UserTable;
 use App\View\PhpTemplateEngine;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,34 +17,38 @@ use Twig\Loader\FilesystemLoader;
 
 class StorefrontController extends AbstractController
 {
-    private const HTTP_STATUS_303_SEE_OTHER = 303; 
+    private const HTTP_STATUS_303_SEE_OTHER = 303;
     private PizzaTable $pizzaTable;
+    private UserTable $userTable;
     private Environment $twig;
 
     public function __construct()
     {
         $connection = ConnectionProvider::connectDatabase();
         $this->pizzaTable = new PizzaTable($connection);
+        $this->userTable = new UserTable($connection);
         $this->twig = new Environment(new FilesystemLoader("../templates"));
     }
 
     public function showHome(): Response
     {
+        session_start();
+        if (!isset($_SESSION['email']))
+        {
+            return $this->redirectToRoute("login", [], Response::HTTP_SEE_OTHER);
+        }
+        $user = $this->userTable->findUser($_SESSION['email']);
         $pizzas = $this->pizzaTable->getAllPizzas();
         $contents = $this->twig->render("./pages/home.html.twig", [
+
             "sections" => [
                 [
                     "section_name" => "hit",
                     "pizzas" => $pizzas
                 ]
-            ]
+            ],
+            "user" => $user,
         ]);
         return new Response($contents);
     }
-
-    private function writeRedirectSeeOther(string $url): void
-    {
-        header("Location: " . $url, true, self::HTTP_STATUS_303_SEE_OTHER);
-    }
-
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
@@ -9,6 +10,7 @@ use App\Model\User;
 use App\Uploader\Uploader;
 use App\View\PhpTemplateEngine;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ErrorHandler\Error\FatalError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
@@ -17,7 +19,7 @@ use Twig\Loader\FilesystemLoader;
 class UserController extends AbstractController
 {
     private const HTTP_STATUS_303_SEE_OTHER = 303;
-    private UserTable $userTable;   
+    private UserTable $userTable;
     private Uploader $uploader;
     private Environment $twig;
 
@@ -43,28 +45,32 @@ class UserController extends AbstractController
 
     public function registerUser(Request $request): Response
     {
-        $requestData = $_POST;
-        $imageData = $_FILES;
-        if ($_SERVER["REQUEST_METHOD"] !== "POST")
-        {
-            $this->writeRedirectSeeOther("/");
-            return new Response();
-        }
+        session_start();
 
+        $imageData = $_FILES;
         $avatar_path = null;
-        if (isset($imageData["avatar"]) & $imageData["avatar"]["name"] != "")
-        {
+        if (isset($imageData["avatar"]) & $imageData["avatar"]["name"] != "") {
             $avatar_path = $this->uploader->moveAvatarToUploads($imageData["avatar"]);
         }
+
         $user = new User(
-            email: $requestData["email"],
-            password: $requestData["password"],
-            name: $requestData["name"],
-            lastname: $requestData["lastname"],
-            address: $requestData["address"],
+            email: $request->get("email"),
+            password: $request->get("password"),
+            name: $request->get("name"),
+            lastname: $request->get("lastname"),
+            address: $request->get("address"),
             avatarPath: $avatar_path,
         );
 
+        if (!$this->isCorrectRegistrationData($user)) {
+            return $this->redirectToRoute("registration", [], Response::HTTP_SEE_OTHER);
+        }
+
+        if (!$this->isNewUserEmail($user->getEmail())){
+            return $this->redirectToRoute("registration", [], Response::HTTP_SEE_OTHER);
+        }
+
+        $_SESSION['email'] = $user->getEmail();
         $this->userTable->saveUser($user);
 
         return $this->redirectToRoute("show_home", [], Response::HTTP_SEE_OTHER);
@@ -72,12 +78,46 @@ class UserController extends AbstractController
 
     public function loginUser(Request $request): Response
     {
+        session_start();
+
+        $userEmail = $request->get("email");
+        $userPassword = $request->get("password");
+
+        if (!$this->isCorrectLoginData($userEmail, $userPassword)) {
+            return $this->redirectToRoute("login", [], Response::HTTP_SEE_OTHER);
+        }
+
+        $_SESSION["email"] = $userEmail;
         return $this->redirectToRoute("show_home", [], Response::HTTP_SEE_OTHER);
+    }
+
+    private function isCorrectLoginData(string $email, string $password): bool
+    {
+        $user = $this->userTable->findUser($email);
+        return $user->getPassword() == $password;
+    }
+
+    private function isNewUserEmail(string $email): bool
+    {
+        return $this->userTable->findUser($email) === null;
+    }
+
+    private function isCorrectRegistrationData(User $user): bool
+    {
+
+        $email = $user->getEmail();
+        if ($email === null) {
+            return false;
+        }
+        $emailValidationRegex = "/^\\S+@\\S+\\.\\S+$/";
+        if (!preg_match($emailValidationRegex, $email)) {
+            return false;
+        }
+        return true;
     }
 
     private function writeRedirectSeeOther(string $url): void
     {
         header("Location: " . $url, true, self::HTTP_STATUS_303_SEE_OTHER);
     }
-
 }
