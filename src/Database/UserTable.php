7f2065a1224a01680e3ef4203998cb069d3d1c3b
QwErTy-yTrEwQ03 <maks-veselov-2003@mail.ru>
2023-05-06 19:07:21 +0300

    Добавил класс для таблицы user

---
+++ src/Database/UserTable.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Database;
+
+use App\Model\User;
+
+class UserTable
+{
+    private \PDO $connection;
+
+    public function __construct(\PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function findUser(string $email): ?User
+    {
+        $query = <<< SQL
+            SELECT
+                email, name, lastname, address, avatar_path
+            FROM user
+            WHERE email = $email
+        SQL;
+
+        $statement = $this->connection->query($query);
+        if ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            return $this->createUserFromRow($row);
+        }
+
+        return null;
+    }
+
+    function saveUser(User $user)
+    {
+        $query = <<< SQL
+            INSERT INTO user (email, password, name, lastname, address, avatar_path)
+            VALUES (:email, :password, :name, :lastname, :address, :avatar_path)
+        SQL;
+
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute([
+            ":email" => $user->getEmail(),
+            ":password" => $user->getPassword(),
+            ":name" => $user->getName(),
+            ":lastname" => $user->getLastName(),
+            ":address" => $user->getAddress(),
+            ":avatar_path" => $user->getAvatarPath(),
+        ]);
+    }
+
+    private function createUserFromRow(array $row): User
+    {
+        return new User(
+            email: $row["email"],
+            password: $row["password"],
+            name: $row["name"],
+            lastname: $row["lastname"],
+            address: $row["address"],
+            avatarPath: $row["avatar_path"],
+        );
+    }
+}
