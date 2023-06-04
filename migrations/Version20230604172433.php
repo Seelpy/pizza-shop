4c5f8bc48cf7540e0eb87dcd0785695186ca48cf
QwErTy-yTrEwQ03 <maks-veselov-2003@mail.ru>
2023-06-05 00:06:24 +0300

    СОздал миграции(добавление поля id у user)

---
+++ migrations/Version20230604172433.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230604172433 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add id in user';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+        $this->addSql('CREATE TABLE user
+        (
+          id INT UNSIGNED AUTO_INCREMENT,
+          email VARCHAR(200) NOT NULL,
+          password VARCHAR(50) NOT NULL,
+          name VARCHAR(100) NOT NULL,
+          lastname VARCHAR(100),
+          address VARCHAR(300),
+          avatar_path VARCHAR(100),
+          PRIMARY KEY (id)
+        );');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+        $this->addSql('CREATE TABLE user (email VARCHAR(200) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, password VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, name VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, lastname VARCHAR(100) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, address VARCHAR(300) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, avatar_path VARCHAR(100) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, PRIMARY KEY(email)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+    }
+}
