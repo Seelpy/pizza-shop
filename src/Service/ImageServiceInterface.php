1462cf6c024c0012c29099950d4af4bf117b1f66
QwErTy-yTrEwQ03 <maks-veselov-2003@mail.ru>
2023-06-07 21:50:50 +0300

    Интерфейс для ImageService

---
+++ src/Service/ImageServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+
+interface ImageServiceInterface
+{
+    public function moveImageToUploads(array $fileInfo): ?string;
+
+    public function getUploadUrlPath(string $fileName): ?string;
+}
